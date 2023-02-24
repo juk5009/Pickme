d9a41849e8b92d2bc1ec5f1b599c752bd5304daf
wonbin3366 <wonbin3366@naver.com>
2023-02-24 15:20:01 +0900

    기업회원가입 view 완료

---
+++ src/main/java/shop/mtcoding/pickme/controller/CompanyController.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.pickme.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CompanyController {
+
+    @GetMapping("/companyJoinForm")
+    public String companyJoinForm() {
+        return "company/companyJoinForm";
+    }
+}
